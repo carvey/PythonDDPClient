61d24a8c16fdf2931a111040530f81d4fd41b8a6
Alex Channell <achannell@morristechnology.com>
2016-04-13 23:48:56 -0400

    graphing in real time

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from src.DDPClient import DDPClient
 from pprint import pprint
+from random import randint
 
 client = DDPClient('ws://localhost:3000/websocket')
 response = client.receive()
@@ -9,15 +10,16 @@ response = client.receive()
 print(response)
 
 # Test the connection with some ping/pong heartbeats
-# import time
+import time
 # time.sleep(1)
-# for i in range(10):
-#     client.ping(str(i))
-#     response = client.receive()
-#     print(response)
-#     time.sleep(2)
+for i in range(30):
+    # client.ping(str(i))
+    # response = client.receive()
+    # print(response)
+    client.call("addPyData", randint(0, 100))
+    time.sleep(.1)
 
-client.call("addPyData", 15)
+
 
 response = client.receive()
 print(response)
