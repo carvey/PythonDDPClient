2456ef5923e562671ab67472b0849b7bc54afc78
Alex Channell <achannell@morristechnology.com>
2016-04-20 16:02:07 -0400

    sleep .1 -> sleep 1

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ print(response)
 # simulate receiving data and calling the JS sending the information to the server which calls a JS function
 for i in range(randint(0, 10)):
     client.call("addPyData", randint(0, 100))
-    time.sleep(.1)
+    time.sleep(1)
 
 response = client.receive()
 print(response)
