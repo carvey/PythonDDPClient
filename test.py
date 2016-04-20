cd8d89c1392a7209892d4055f3ceb71d9d00f9b6
carvey <j95.charles@gmail.com>
2016-04-20 15:51:14 -0400

    Merge branch 'addCupcakes' of /Users/charlesarvey/PycharmProjects/PythonDDPClient with conflicts.

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import time
 from src.DDPClient import DDPClient
+from pprint import pprint
 from random import randint
 
 client = DDPClient('ws://localhost:3000/websocket')
@@ -9,10 +9,16 @@ print(response)
 response = client.receive()
 print(response)
 
-# simulate receiving data and calling the JS sending the information to the server which calls a JS function
-for i in range(randint(0, 10)):
-    client.call("addPyData", randint(0, 100))
-    time.sleep(.1)
+# Test the connection with some ping/pong heartbeats
+import time
+# time.sleep(1)
+for i in range(3):
+    # client.ping(str(i))
+    # response = client.receive()
+    # print(response)
+    client.call("addPyData", randint(0, 10))
+    response = client.receive()
+    print(response)
+    time.sleep(2)
 
-response = client.receive()
-print(response)
+
